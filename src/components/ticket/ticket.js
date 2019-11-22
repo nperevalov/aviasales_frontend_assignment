23068c15cf74bf6b0b66e255c52f3c61ccc7c0b3
Nikolay Perevalov <nnperevalov@gmail.com>
2019-11-22 23:00:44 +0300

    First grid commit

--- src/components/ticket/ticket.js
+++ src/components/ticket/ticket.js
@@ -9,33 +9,30 @@ export default function Ticket() {
         <div className="TicketSpacer"></div>
         <div className="TicketCompany"></div>
       </div>
-      <div className="TicketRow">
-        <div className="TicketRoute">
-          <div className="TicketRowHeader">MOW – HKT</div>
-          <div className="TicketRowValue">10:45 – 08:00</div>
-        </div>
-        <div className="TicketLenght">
-          <div className="TicketRowHeader">В пути</div>
-          <div className="TicketRowValue">21ч 15м</div>
-        </div>
-        <div className="TicketStops">
-          <div className="TicketRowHeader">2 пересадки</div>
-          <div className="TicketRowValue">HKG, JNB</div>
-        </div>
+
+      <div className="TicketRoute">
+        <div className="TicketRowHeader">MOW – HKT</div>
+        <div className="TicketRowValue">10:45 – 08:00</div>
       </div>
-      <div className="TicketRow">
-        <div className="TicketRoute">
-          <div className="TicketRowHeader">MOW – HKT</div>
-          <div className="TicketRowValue">10:45 – 08:00</div>
-        </div>
-        <div className="TicketLenght">
-          <div className="TicketRowHeader">В пути</div>
-          <div className="TicketRowValue">21ч 15м</div>
-        </div>
-        <div className="TicketStops">
-          <div className="TicketRowHeader">2 пересадки</div>
-          <div className="TicketRowValue">HKG, JNB</div>
-        </div>
+      <div className="TicketLenght">
+        <div className="TicketRowHeader">В пути</div>
+        <div className="TicketRowValue">21ч 15м</div>
+      </div>
+      <div className="TicketStops">
+        <div className="TicketRowHeader">2 пересадки</div>
+        <div className="TicketRowValue">HKG, JNB</div>
+      </div>
+      <div className="TicketRoute">
+        <div className="TicketRowHeader">MOW – HKT</div>
+        <div className="TicketRowValue">10:45 – 08:00</div>
+      </div>
+      <div className="TicketLenght">
+        <div className="TicketRowHeader">В пути</div>
+        <div className="TicketRowValue">21ч 15м</div>
+      </div>
+      <div className="TicketStops">
+        <div className="TicketRowHeader">2 пересадки</div>
+        <div className="TicketRowValue">HKG, JNB</div>
       </div>
     </div>
   );
